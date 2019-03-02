302c1678913546af33169d6950065b6dd7840bb4
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-03-02 19:56:52 +0100

    changes, does not work

--- script-compiled.js
+++ script-compiled.js
@@ -1,106 +1,140 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Stopwatch = function () {
-    function Stopwatch(display) {
-        _classCallCheck(this, Stopwatch);
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Stopwatch = function (_React$Component) {
+  _inherits(Stopwatch, _React$Component);
+
+  function Stopwatch(props) {
+    _classCallCheck(this, Stopwatch);
+
+    var _this = _possibleConstructorReturn(this, (Stopwatch.__proto__ || Object.getPrototypeOf(Stopwatch)).call(this, props));
+
+    _this.state = {
+      running: false,
+      display: display,
+      times: {
+        minutes: 0,
+        seconds: 0,
+        miliseconds: 0
+      },
+      results: [] //nie do konca rozumiem
+    };
+    return _this;
+  }
+
+  _createClass(Stopwatch, [{
+    key: "reset",
+    value: function reset() {
+      this.setState({
+        times: {
+          minutes: 0,
+          seconds: 0,
+          miliseconds: 0
+        }
+      });
     }
+  }, {
+    key: "format",
+    value: function format(times) {
+      return pad0(times.minutes) + ":" + pad0(times.seconds) + ":" + pad0(Math.floor(times.miliseconds));
+    }
+  }, {
+    key: "start",
+    value: function start() {
+      if (!this.state.running) {
+        this.setState({
+          running: true
+        }), this.watch = setInterval(this.step.bind(this), 10);
+      }
+    }
+  }, {
+    key: "step",
+    value: function step() {
+      if (!this.state.running) return;
+      this.calculate();
+    }
+  }, {
+    key: "calculate",
+    value: function calculate() {
+      var times = this.state.times;
+      times.miliseconds += 1;
+      if (times.miliseconds >= 100) {
+        times.seconds += 1;
+        times.miliseconds = 0;
+      }
+      if (times.seconds >= 60) {
+        times.minutes += 1;
+        times.seconds = 0;
+      }
+      this.setState({
+        times: times
+      });
+    }
+  }, {
+    key: "stop",
+    value: function stop() {
+      this.setState({
+        running: false
+      }), clearInterval(this.watch);
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
 
-    _createClass(Stopwatch, [{
-        key: 'reset',
-        value: function reset() {
-            this.times = {
-                minutes: 0,
-                seconds: 0,
-                miliseconds: 0
-            };
-        }
-    }, {
-        key: 'print',
-        value: function print() {
-            //ok
-            this.display.innerText = this.format(this.times);
-        }
-    }, {
-        key: 'format',
-        value: function format(times) {
-            //przygotowuje tekst do wyswietlenia
-            return pad0(times.minutes) + ':' + pad0(times.seconds) + ':' + pad0(Math.floor(times.miliseconds));
-        } //wynik to 02:04:23 (2 min, 4 s, 10 ms)
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "nav",
+          { className: "controls" },
+          React.createElement(
+            "a",
+            {
+              href: "#",
+              className: "button",
+              id: "start",
+              onClick: function onClick() {
+                return _this2.start();
+              } },
+            "Start",
+            " "
+          ),
+          React.createElement(
+            "a",
+            {
+              href: "#",
+              className: "button",
+              id: "stop",
+              onClick: function onClick() {
+                return _this2.stop();
+              } },
+            "Stop",
+            " "
+          )
+        )
+      );
+    }
+  }]);
 
-    }, {
-        key: 'start',
-        value: function start() {
-            var _this = this;
+  return Stopwatch;
+}(React.Component);
 
-            //ok
-            if (!this.running) {
-                this.running = true;
-                this.watch = setInterval(function () {
-                    return _this.step();
-                }, 10);
-            }
-        }
-    }, {
-        key: 'step',
-        value: function step() {
-            //ok
-            if (!this.running) return;
-            this.calculate();
-            this.print();
-        }
-    }, {
-        key: 'calculate',
-        value: function calculate() {
-            //ok
-            this.times.miliseconds += 1;
-            if (this.times.miliseconds >= 100) {
-                this.times.seconds += 1;
-                this.times.miliseconds = 0;
-            }
-            if (this.times.seconds >= 60) {
-                this.times.minutes += 1;
-                this.times.seconds = 0;
-            }
-        }
-    }, {
-        key: 'stop',
-        value: function stop() {
-            //ok
-            this.running = false;
-            clearInterval(this.watch);
-        }
-    }]);
+var stopwatch = React.createElement(Stopwatch);
+ReactDOM.render(stopwatch, document.getElementsByClassName("container"));
+//ReactDOM.render(<Stopwatch />, document.getElementById("container"));
 
-    return Stopwatch;
-}();
-
-var stopwatch = new Stopwatch(document.querySelector('.stopwatch')); //ok
-
-var startButton = document.getElementById('start');
-startButton.addEventListener('click', function () {
-    return stopwatch.start();
-}); //ok
-
-var stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', function () {
-    return stopwatch.stop();
-}); //ok
-
-//Funkcja pad0 przyjmuje na wejście wartość liczbową, przekształca ją na stringa, a następnie sprawdza czy długość tego przekształcenia jest mniejsza od 2 dodając tym samym zero przed tę liczbę.
 function pad0(value) {
-    //dodaje 0 do liczb jednocyfrowych
-    var result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
+  var result = value.toString();
+  if (result.length < 2) {
+    result = "0" + result;
+  }
+  return result;
 }
--- script.js
+++ script.js
@@ -1,95 +1,101 @@
-//const { minutes, seconds, miliseconds } = times;
+class Stopwatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      running: false,
+      display: display,
+      times: {
+        minutes: 0,
+        seconds: 0,
+        miliseconds: 0
+      },
+      results: [] //nie do konca rozumiem
+    };
+  }
+  reset() {
+    this.setState({
+      times: {
+        minutes: 0,
+        seconds: 0,
+        miliseconds: 0
+      }
+    });
+  }
 
+  format(times) {
+    return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(
+      Math.floor(times.miliseconds)
+    )}`;
+  }
 
-class Stopwatch {
-    constructor(display) {
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
+  start() {
+    if (!this.state.running) {
+      this.setState({
+        running: true
+      }),
+        (this.watch = setInterval(this.step.bind(this), 10));
     }
+  }
 
-    reset() {
-        this.times = {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0
-        };
+  step() {
+    if (!this.state.running) return;
+    this.calculate();
+  }
+
+  calculate() {
+    var times = this.state.times;
+    times.miliseconds += 1;
+    if (times.miliseconds >= 100) {
+      times.seconds += 1;
+      times.miliseconds = 0;
     }
-    print() { //ok
-        this.display.innerText = this.format(this.times);
+    if (times.seconds >= 60) {
+      times.minutes += 1;
+      times.seconds = 0;
     }
+    this.setState({
+      times
+    });
+  }
 
-    // format() {
-    //     this.times(pad0) = {
-    //         minutes,
-    //         seconds,
-    //         miliseconds
-    //     };
-    // }
-    format(times) { //przygotowuje tekst do wyswietlenia
-        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-    } //wynik to 02:04:23 (2 min, 4 s, 10 ms)
+  stop() {
+    this.setState({
+      running: false
+    }),
+      clearInterval(this.watch);
+  }
 
-    start() { //ok
-        if (!this.running) {
-            this.running = true;
-            this.watch = setInterval(() => this.step(), 10);
-        }
-    }
-
-    step() { //ok
-        if (!this.running) return;
-        this.calculate();
-        this.print();
-    }
-
-    // calculate() {
-    //     this.times = {
-    //         miliseconds = +1,
-    //         if (this.miliseconds >= 100) {
-    //             seconds = +1,
-    //                 miliseconds = 0;
-    //         }
-    //         if (this.times.seconds >= 60) {
-    //             minutes = +1,
-    //                 seconds = 0,
-    //         }
-    //     }
-    // }
-
-
-
-    calculate() { //ok
-        this.times.miliseconds += 1;
-        if (this.times.miliseconds >= 100) {
-            this.times.seconds += 1;
-            this.times.miliseconds = 0;
-        }
-        if (this.times.seconds >= 60) {
-            this.times.minutes += 1;
-            this.times.seconds = 0;
-        }
-    }
-
-    stop() { //ok
-        this.running = false;
-        clearInterval(this.watch);
-    }
+  render() {
+    return (
+      <div>
+        <nav className={"controls"}>
+          <a
+            href={"#"}
+            className={"button"}
+            id={"start"}
+            onClick={() => this.start()}>
+            Start{" "}
+          </a>
+          <a
+            href={"#"}
+            className={"button"}
+            id={"stop"}
+            onClick={() => this.stop()}>
+            Stop{" "}
+          </a>
+        </nav>
+      </div>
+    );
+  }
 }
-const stopwatch = new Stopwatch(document.querySelector('.stopwatch')); //ok
+var stopwatch = React.createElement(Stopwatch);
+ReactDOM.render(stopwatch, document.getElementsByClassName("container"));
+//ReactDOM.render(<Stopwatch />, document.getElementById("container"));
 
-let startButton = document.getElementById('start');
-startButton.addEventListener('click', () => stopwatch.start()); //ok
-
-let stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => stopwatch.stop()); //ok
-
-//Funkcja pad0 przyjmuje na wejście wartość liczbową, przekształca ją na stringa, a następnie sprawdza czy długość tego przekształcenia jest mniejsza od 2 dodając tym samym zero przed tę liczbę.
-function pad0(value) { //dodaje 0 do liczb jednocyfrowych
-    let result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
+function pad0(value) {
+  let result = value.toString();
+  if (result.length < 2) {
+    result = "0" + result;
+  }
+  return result;
 }
